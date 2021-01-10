8a4171eca795bd1a5f14ce9ee81404da30bb8010
satori87 <satori87@gmail.com>
2021-01-09 19:43:41 -0500

    commission

--- editor/mudeditor/FormEditor.cs
+++ editor/mudeditor/FormEditor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,31 +9,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace mudeditor
-{
-    public partial class FormEditor : Form
-    {
-        public FormEditor()
-        {
+namespace mudeditor {
+    public partial class FormEditor : Form {
+        public FormEditor() {
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             Font = new Font(Font.Name, 8.25f * 96f / CreateGraphics().DpiX, Font.Style, Font.Unit, Font.GdiCharSet, Font.GdiVerticalFont);
             this.AutoScaleMode = AutoScaleMode.None;
             InitializeComponent();
         }
 
-        private void FormAreaList_Load(object sender, EventArgs e)
-        {
+        private void FormAreaList_Load(object sender, EventArgs e) {
 
         }
 
-        private void txtFetchArea_Click(object sender, EventArgs e)
-        {
+        private void txtFetchArea_Click(object sender, EventArgs e) {
             fetchArea();
         }
 
-        void fetchArea()
-        {
+        void fetchArea() {
+            String cs = @"server=18.223.190.165;port=3306;userid=bear;password=%Pb?fYW@ydP9RLqeTnfSW-u!23c$f=%#;database=mud";
+            var con = new MySqlConnection(cs);
+            con.Open();
+            var stm = "SELECT name,json FROM area";
+            var cmd = new MySql.Data.MySqlClient.MySqlCommand(stm, con);
+            MySql.Data.MySqlClient.MySqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read()) {
+                Console.WriteLine("aa");
+                Console.WriteLine(rdr.GetString(0) + " " + rdr.GetString(1));
+            }
+            //Console.WriteLine(result.ToString());
+            //,;
 
+            lstArea.Items.Clear();
         }
     }
 }
