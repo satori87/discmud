7456581d2b5f12528f48637dd4f7a057380a1086
satori87 <satori87@gmail.com>
2021-01-13 22:12:25 -0500

    stuff

--- editor/mudeditor/FormMonster.Designer.cs
+++ editor/mudeditor/FormMonster.Designer.cs
@@ -23,9 +23,9 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMonster));
             this.tab = new System.Windows.Forms.TabControl();
             this.tabMain = new System.Windows.Forms.TabPage();
-            this.tabBehavior = new System.Windows.Forms.TabPage();
             this.txtMagDef = new System.Windows.Forms.TextBox();
             this.txtMagEva = new System.Windows.Forms.TextBox();
             this.txtPhysDef = new System.Windows.Forms.TextBox();
@@ -41,6 +41,7 @@
             this.txtDisplayName = new System.Windows.Forms.TextBox();
             this.txtName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.tabBehavior = new System.Windows.Forms.TabPage();
             this.tabLoot = new System.Windows.Forms.TabPage();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
@@ -57,7 +58,7 @@
             this.tab.Location = new System.Drawing.Point(0, 0);
             this.tab.Name = "tab";
             this.tab.SelectedIndex = 0;
-            this.tab.Size = new System.Drawing.Size(871, 635);
+            this.tab.Size = new System.Drawing.Size(871, 661);
             this.tab.TabIndex = 15;
             // 
             // tabMain
@@ -80,122 +81,135 @@
             this.tabMain.Location = new System.Drawing.Point(4, 38);
             this.tabMain.Name = "tabMain";
             this.tabMain.Padding = new System.Windows.Forms.Padding(3);
-            this.tabMain.Size = new System.Drawing.Size(863, 593);
+            this.tabMain.Size = new System.Drawing.Size(863, 619);
             this.tabMain.TabIndex = 0;
             this.tabMain.Text = "Stats";
             this.tabMain.UseVisualStyleBackColor = true;
             // 
-            // tabBehavior
-            // 
-            this.tabBehavior.Location = new System.Drawing.Point(4, 29);
-            this.tabBehavior.Name = "tabBehavior";
-            this.tabBehavior.Padding = new System.Windows.Forms.Padding(3);
-            this.tabBehavior.Size = new System.Drawing.Size(671, 602);
-            this.tabBehavior.TabIndex = 1;
-            this.tabBehavior.Text = "Behavior";
-            this.tabBehavior.UseVisualStyleBackColor = true;
-            // 
             // txtMagDef
             // 
+            this.txtMagDef.AccessibleName = "int";
             this.txtMagDef.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMagDef.Location = new System.Drawing.Point(228, 424);
+            this.txtMagDef.Location = new System.Drawing.Point(228, 426);
             this.txtMagDef.Name = "txtMagDef";
             this.txtMagDef.Size = new System.Drawing.Size(100, 30);
             this.txtMagDef.TabIndex = 29;
+            this.txtMagDef.Leave += new System.EventHandler(this.txtLeave);
             // 
             // txtMagEva
             // 
+            this.txtMagEva.AccessibleName = "int";
             this.txtMagEva.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMagEva.Location = new System.Drawing.Point(228, 392);
+            this.txtMagEva.Location = new System.Drawing.Point(228, 394);
             this.txtMagEva.Name = "txtMagEva";
             this.txtMagEva.Size = new System.Drawing.Size(100, 30);
             this.txtMagEva.TabIndex = 28;
+            this.txtMagEva.Leave += new System.EventHandler(this.txtLeave);
             // 
             // txtPhysDef
             // 
+            this.txtPhysDef.AccessibleName = "int";
             this.txtPhysDef.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPhysDef.Location = new System.Drawing.Point(228, 360);
+            this.txtPhysDef.Location = new System.Drawing.Point(228, 362);
             this.txtPhysDef.Name = "txtPhysDef";
             this.txtPhysDef.Size = new System.Drawing.Size(100, 30);
             this.txtPhysDef.TabIndex = 27;
+            this.txtPhysDef.Leave += new System.EventHandler(this.txtLeave);
             // 
             // txtPhysEva
             // 
+            this.txtPhysEva.AccessibleName = "int";
             this.txtPhysEva.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPhysEva.Location = new System.Drawing.Point(228, 328);
+            this.txtPhysEva.Location = new System.Drawing.Point(228, 330);
             this.txtPhysEva.Name = "txtPhysEva";
             this.txtPhysEva.Size = new System.Drawing.Size(100, 30);
             this.txtPhysEva.TabIndex = 26;
+            this.txtPhysEva.Leave += new System.EventHandler(this.txtLeave);
             // 
             // txtSpeed
             // 
+            this.txtSpeed.AccessibleName = "int";
             this.txtSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSpeed.Location = new System.Drawing.Point(228, 296);
+            this.txtSpeed.Location = new System.Drawing.Point(228, 298);
             this.txtSpeed.Name = "txtSpeed";
             this.txtSpeed.Size = new System.Drawing.Size(100, 30);
             this.txtSpeed.TabIndex = 25;
+            this.txtSpeed.Leave += new System.EventHandler(this.txtLeave);
             // 
             // txtLuck
             // 
+            this.txtLuck.AccessibleName = "int";
             this.txtLuck.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtLuck.Location = new System.Drawing.Point(228, 264);
+            this.txtLuck.Location = new System.Drawing.Point(228, 266);
             this.txtLuck.Name = "txtLuck";
             this.txtLuck.Size = new System.Drawing.Size(100, 30);
             this.txtLuck.TabIndex = 24;
+            this.txtLuck.Leave += new System.EventHandler(this.txtLeave);
             // 
             // txtWis
             // 
+            this.txtWis.AccessibleName = "int";
             this.txtWis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtWis.Location = new System.Drawing.Point(228, 232);
+            this.txtWis.Location = new System.Drawing.Point(228, 234);
             this.txtWis.Name = "txtWis";
             this.txtWis.Size = new System.Drawing.Size(100, 30);
             this.txtWis.TabIndex = 23;
+            this.txtWis.Leave += new System.EventHandler(this.txtLeave);
             // 
             // txtInt
             // 
+            this.txtInt.AccessibleName = "int";
             this.txtInt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtInt.Location = new System.Drawing.Point(228, 200);
+            this.txtInt.Location = new System.Drawing.Point(228, 202);
             this.txtInt.Name = "txtInt";
             this.txtInt.Size = new System.Drawing.Size(100, 30);
             this.txtInt.TabIndex = 22;
+            this.txtInt.Leave += new System.EventHandler(this.txtLeave);
             // 
             // txtCon
             // 
+            this.txtCon.AccessibleName = "int";
             this.txtCon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCon.Location = new System.Drawing.Point(228, 168);
+            this.txtCon.Location = new System.Drawing.Point(228, 170);
             this.txtCon.Name = "txtCon";
             this.txtCon.Size = new System.Drawing.Size(100, 30);
             this.txtCon.TabIndex = 21;
+            this.txtCon.Leave += new System.EventHandler(this.txtLeave);
             // 
             // txtDex
             // 
+            this.txtDex.AccessibleName = "int";
             this.txtDex.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDex.Location = new System.Drawing.Point(228, 136);
+            this.txtDex.Location = new System.Drawing.Point(228, 138);
             this.txtDex.Name = "txtDex";
             this.txtDex.Size = new System.Drawing.Size(100, 30);
             this.txtDex.TabIndex = 20;
+            this.txtDex.Leave += new System.EventHandler(this.txtLeave);
             // 
             // txtStr
             // 
+            this.txtStr.AccessibleName = "int";
             this.txtStr.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtStr.Location = new System.Drawing.Point(228, 104);
+            this.txtStr.Location = new System.Drawing.Point(228, 106);
             this.txtStr.Name = "txtStr";
             this.txtStr.Size = new System.Drawing.Size(100, 30);
             this.txtStr.TabIndex = 19;
+            this.txtStr.Leave += new System.EventHandler(this.txtLeave);
             // 
             // txtHP
             // 
             this.txtHP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtHP.Location = new System.Drawing.Point(228, 72);
+            this.txtHP.Location = new System.Drawing.Point(228, 74);
             this.txtHP.Name = "txtHP";
             this.txtHP.Size = new System.Drawing.Size(100, 30);
             this.txtHP.TabIndex = 18;
             this.txtHP.Text = "1d1+1";
+            this.txtHP.Leave += new System.EventHandler(this.txtHP_Leave);
             // 
             // txtDisplayName
             // 
             this.txtDisplayName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDisplayName.Location = new System.Drawing.Point(228, 40);
+            this.txtDisplayName.Location = new System.Drawing.Point(228, 42);
             this.txtDisplayName.Name = "txtDisplayName";
             this.txtDisplayName.Size = new System.Drawing.Size(414, 30);
             this.txtDisplayName.TabIndex = 17;
@@ -203,7 +217,7 @@
             // txtName
             // 
             this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtName.Location = new System.Drawing.Point(228, 8);
+            this.txtName.Location = new System.Drawing.Point(228, 10);
             this.txtName.Name = "txtName";
             this.txtName.ReadOnly = true;
             this.txtName.Size = new System.Drawing.Size(414, 30);
@@ -212,20 +226,29 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(29, 8);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(191, 448);
+            this.label1.Size = new System.Drawing.Size(226, 448);
             this.label1.TabIndex = 15;
-            this.label1.Text = "Name\r\nDisplay Name\r\nHP Dice\r\nStrength\r\nDexterity\r\nConstitution\r\nIntelligence\r\nWis" +
-    "dom\r\nLuck\r\nTurn Speed\r\nPhys Eva\r\nPhys Def\r\nMag Eva\r\nMag Def";
+            this.label1.Text = resources.GetString("label1.Text");
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tabBehavior
+            // 
+            this.tabBehavior.Location = new System.Drawing.Point(4, 38);
+            this.tabBehavior.Name = "tabBehavior";
+            this.tabBehavior.Padding = new System.Windows.Forms.Padding(3);
+            this.tabBehavior.Size = new System.Drawing.Size(863, 619);
+            this.tabBehavior.TabIndex = 1;
+            this.tabBehavior.Text = "Behavior";
+            this.tabBehavior.UseVisualStyleBackColor = true;
             // 
             // tabLoot
             // 
-            this.tabLoot.Location = new System.Drawing.Point(4, 29);
+            this.tabLoot.Location = new System.Drawing.Point(4, 38);
             this.tabLoot.Name = "tabLoot";
-            this.tabLoot.Size = new System.Drawing.Size(671, 602);
+            this.tabLoot.Size = new System.Drawing.Size(863, 619);
             this.tabLoot.TabIndex = 2;
             this.tabLoot.Text = "Loot";
             this.tabLoot.UseVisualStyleBackColor = true;
@@ -233,20 +256,21 @@
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(490, 641);
+            this.btnSave.Location = new System.Drawing.Point(490, 551);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(156, 68);
             this.btnSave.TabIndex = 16;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnCancel
             // 
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(12, 641);
+            this.btnCancel.Location = new System.Drawing.Point(16, 551);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(156, 68);
-            this.btnCancel.TabIndex = 17;
+            this.btnCancel.TabIndex = 18;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
@@ -255,7 +279,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(658, 721);
+            this.ClientSize = new System.Drawing.Size(658, 628);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.tab);
@@ -265,6 +289,7 @@
             this.Text = "Editing Monster";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMonster_FormClosing);
             this.Load += new System.EventHandler(this.FormMonster_Load);
+            this.Shown += new System.EventHandler(this.FormMonster_Shown);
             this.tab.ResumeLayout(false);
             this.tabMain.ResumeLayout(false);
             this.tabMain.PerformLayout();
--- editor/mudeditor/FormMonster.cs
+++ editor/mudeditor/FormMonster.cs
@@ -28,5 +28,76 @@ namespace MUDEdit {
             this.Hide();
             MUDEdit.formEditor.Show();
         }
+
+        private void btnSave_Click(object sender, EventArgs e) {
+
+        }
+
+        private void txtHP_Leave(object sender, EventArgs e) {
+            fix();            
+        }
+
+        void fix() {
+            //fix hp dice, TODO move to function somewhere nice and static
+            String s = txtHP.Text;
+            int numDice = 1;
+            int numSides = 1;
+            int bonus = 0;
+            try {
+                int dpos = s.IndexOf("d");
+                String num = s.Substring(0, dpos);
+                String rest = s.Substring(dpos);
+                numDice = int.Parse(num);
+                int ppos = rest.IndexOf("+");
+                String sides = rest;
+                if (ppos > 0) {
+                    sides = rest.Substring(0, ppos);
+                    String sbonus = rest.Substring(ppos);
+                    bonus = int.Parse(sbonus);
+                }
+                numSides = int.Parse(sides);
+                String dice = numDice + "d" + numSides + "+" + bonus;
+                txtHP.Text = dice;
+            } catch (Exception ex) {
+                Console.WriteLine(ex);
+                txtHP.Text = "";
+            }
+
+            foreach (Control c in tabMain.Controls) {
+                if (c is TextBox && c.AccessibleName != null && c.AccessibleName.Equals("int")) {
+                    TextBox t = (TextBox)c;
+                    try {
+                        int n = int.Parse(t.Text);
+                    } catch (Exception ex) {
+                        Console.WriteLine(ex);
+                        t.Text = "0";
+                    }
+                }
+            }
+        }
+
+        private void txtLeave(object sender, EventArgs e) {
+            fix();
+        }
+
+        private void FormMonster_Shown(object sender, EventArgs e) {
+            Monster m =  MUDEdit.curMonster;
+            txtName.Text = (String)m.fields["name"];
+            txtDisplayName.Text = (String)m.fields["display_name"];
+            txtHP.Text = (String)m.fields["hp_dice"];
+            txtStr.Text = (String)m.fields["strength"];
+            txtDex.Text = (String)m.fields["dexterity"];
+            txtCon.Text = (String)m.fields["constitution"];
+            txtInt.Text = (String)m.fields["intelligence"];
+            txtWis.Text = (String)m.fields["wisdom"];
+            txtLuck.Text = (String)m.fields["luck"];
+            txtPhysEva.Text = (String)m.fields["physeva"];
+            txtPhysDef.Text = (String)m.fields["physdef"];
+            txtMagEva.Text = (String)m.fields["mageva"];
+            txtMagDef.Text = (String)m.fields["magdef"];
+            txtSpeed.Text = (String)m.fields["speed"];
+
+            fix();
+        }
     }
 }
